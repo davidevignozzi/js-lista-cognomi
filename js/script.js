775e7b78170be957cfc0cde079b1eb59bdf6d1e3
Davide <davide.vignozzi@gmail.com>
2020-12-17 10:23:28 +0100

    CORREZIONE

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 var listaOutput = document.getElementById('lista-cognomi');
-var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi']
-var cognomeUtente = prompt('Inserisci il tuo cognome')
-listaCognomi.push(cognomeUtente)
-listaCognomi.sort()
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+cognomeUtente = cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1).toLowerCase(); // CORREZIONE --> per mettere la prima lettera maiuscola.
+listaCognomi.push(cognomeUtente);
+listaCognomi.sort();
+console.log(listaCognomi);
 
-var contenutoPrecedente;
 for (var i = 0; i < listaCognomi.length; i++) {
-  contenutoPrecedente = listaOutput.innerHTML;
-  listaOutput.innerHTML = contenutoPrecedente + '<li>' + listaCognomi[i] + '</li>';
+  listaOutput.innerHTML += '<li>' + listaCognomi[i] + '</li>';
 }
+
+var userIndex = listaCognomi.indexOf(cognomeUtente) + 1; // CORREZIONE per dire in che posizione si trova il cognomeUtente
+var outputPosition = document.getElementById('output-position') // CORREZIONE
+outputPosition.append(userIndex) // CORREZIONE
